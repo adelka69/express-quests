ed8b1f72fda83903c48a32d85ed8596bcbd2be48
ADEL KARA <karabesch@gmail.com>
2023-12-03 18:04:21 +0100

    quete express4

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -164,6 +164,42 @@ const updateUser = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
@@ -173,4 +209,6 @@ module.exports = {
   postUser,
   updateMovie,
   updateUser,
+  deleteMovie,
+  deleteUser,
 };
